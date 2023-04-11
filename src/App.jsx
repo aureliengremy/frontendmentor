c50c4014bc1f1dc96fc56d9d7bd174bf0f995edc
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-04-10 20:11:56 -0400

    feat(App.jsx) add all partials to the home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useEffect } from 'react';
 import './App.css'
-import Form from './Components/InteractiveRating/Form'
+import Home from './pages/Home'
+import {
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+import Challenge from './pages/Challenge';
 
 function App() {
 
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); // triggered on route change
+ 
+  // TODO: const with array of all challenges to send a list for the footer and maybe the AllChallenges components 
 
   return (
-    <div className="App">
-      <Form />
-    </div>
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/challenge" element={<Challenge />} />
+      </Routes>
+    </>
   )
 }
 
